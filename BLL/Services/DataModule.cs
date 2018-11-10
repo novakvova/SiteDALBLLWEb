0b46bc951d5375aa3318993c79cd89a089a822a4
novakvova <novakvova@gmail.com>
2018-11-10 12:15:43 +0200

    Begin work Bll

---
+++ BLL/Services/DataModule.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using DAL.Abstract;
+using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class DataModule : Module
+    {
+        private string _connStr;
+        private IAppBuilder _app;
+        public DataModule(string connString, IAppBuilder app)
+        {
+            _connStr = connString;
+            _app = app;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => new ApplicationDbContext(this._connStr))
+                .As<IAppDBContext>().InstancePerRequest();
+            builder.Register(ctx =>
+            {
+                var context = (ApplicationDbContext)ctx.Resolve<IAppDBContext>();
+                return context;
+            }).AsSelf().InstancePerDependency();
+            builder.RegisterType<CustomUserStore>()
+                .As<IUserStore<ApplicationUser, int>>().InstancePerRequest();
+            builder.RegisterType<CustomRoleStore>()
+                .As<IRoleStore<CustomRole, int>>().InstancePerRequest();
+
+            base.Load(builder);
+        }
+    }
+}
---
+++ DAL/Concrete/SqlRepository.cs
@@ -0,0 +1,62 @@
+﻿using DAL.Abstract;
+using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Concrete
+{
+    public class SqlRepository : ISqlRepository
+    {
+        private readonly IAppDBContext context;
+
+        public SqlRepository(IAppDBContext context)
+        {
+            this.context = context;
+        }
+
+        private IDbSet<TEntity> GetEntities<TEntity>() where TEntity : class
+        {
+            return this.context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> GetAll<TEntity>() where TEntity : class
+        {
+            return GetEntities<TEntity>().AsQueryable();
+        }
+
+        public void Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            GetEntities<TEntity>().Add(entity);
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            GetEntities<TEntity>().Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            if (this.context != null)
+            {
+                this.context.Dispose();
+            }
+        }
+
+
+        public TEntity GetById<TEntity>(int id) where TEntity : BaseModel<int>
+        {
+            return (from item in GetAll<TEntity>()
+                    where item.Id == id
+                    select item).SingleOrDefault();
+        }
+    }
+}
